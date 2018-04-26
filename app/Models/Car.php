432ac6297c1159ad7b8af9c90ba5ead807083099
Vinicius Lopes <vinicius.ld@outlook.com>
2018-04-25 21:09:37 -0300

    created the resouce controller, model and migration

---
+++ app/Models/Car.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Car extends Model
+{
+    protected $fillable = [
+        'vehicle',
+        'brand',
+        'year',
+        'description',
+        'selled',
+    ];
+}
---
+++ database/migrations/2018_04_25_233519_create_cars_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('vehicle');
+            $table->string('brand');
+            $table->integer('year');
+            $table->text('description');
+            $table->boolean('selled')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
